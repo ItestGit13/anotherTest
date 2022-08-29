45ece2c947bdb8558fbb40966e4f4fdf65823777
Chander <chandershekhar@enacteservices.com>
2022-08-29 13:56:37 +0530

    New Change. Added data in Grid

--- views/site/crud.php
+++ views/site/crud.php
@@ -2,50 +2,66 @@
 use yii\helpers\html;
 use yii\widgets\LinkPager;
 use yii\data\Pagination;
+use yii\data\ActiveDataProvider;
+use app\models\lists;
+use yii\grid\GridView;
 
 $this->title = "Lorem gipsum geneva demo title"
 ?>
 
-<h1>My List</h1>
-<?= html::a('Create List',['create_list'],['class'=>'btn btn-primary','style'=>'margin-bottom : 20px']); ?>
+<?php  $dataProvider = new ActiveDataProvider([
 
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">ID</th>
-      <th scope="col">Title</th>
-      <th scope="col">Description</th>
-      <th scope="col">Cateory</th>
-      <th scope="col">Actions</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php if(count($list) > 0): ?>
-    <?php foreach($list as $lists): ?>
-    <tr>
-      <th scope="row"><?php echo $lists->id; ?></th>
-      <td><?php echo $lists->title; ?></td>
-      <td><?php echo $lists->description; ?></td>
-      <td><?php echo $lists->category; ?></td>
-      <td>
-        <span><?= html::a('View',['view','id'=> $lists->id],['class'=>'btn btn-primary']);?></span>
-        <span><?= html::a('Update',['update','id'=> $lists->id],['class'=>'btn btn-success']);?></span>
-        <span><?= html::a('Delete',['delete','id'=> $lists->id],['class'=>'btn btn-danger']);?></span>
-      </td>
-    </tr>
-    <?php endforeach; ?>
-    <?php else: ?>
-      <div>
-        No Record Found
-      </div>
-      <?php endif; ?>
-  </tbody>
-</table>
-<div>
-<?php
-   // display pagination
-   echo LinkPager::widget([
-      'pagination' => $pagination,
-   ]);
+'query'=>lists::find(),
+'pagination'=> [
+  'pageSize'=>3,
+],
+// 'pagination'=>false,
+  'sort'=>[
+    'attributes'=>['title','description'],
+    // 'defaultOrder'=>['title'=>SORT_ASC, 'description'=> SORT_DESC ]
+  ]
+
+]); ?>
+
+<h1>My List</h1>
+
+<?=
+
+GridView::widget([
+'dataProvider'=>$dataProvider,
+'showFooter'=>true,
+'showHeader'=>true,
+
+'columns'=> [
+  'id',
+  'title',
+  [
+    'attribute'=>'description',
+    'label'=>'Description'
+  ],
+  [
+    'attribute'=>'category'
+  ],
+  [
+    'class'=>'yii\grid\ActionColumn',
+    'header'=>'Actions',
+    'headerOptions'=>['width'=>'80'],
+    'template'=>'{view} {update} {delete}',
+    'visible'=>true,
+    // 'header'=>'Actions',
+    'footer'=>'Testing Status',
+
+  ]
+],
+'layout'=>"\n{summary}\n{items}\n{pager} ",
+'showOnEmpty'=>true,
+
+]);
+
 ?>
-</div>
+
+
+
+
+
+
